c8b1d20a1f78ce33c78d6ea9eb450cd6561e6a64
LikeIvy <ivy74115@gmail.com>
2023-05-18 19:56:12 +0900

    add signin function

--- ExcelCalculate/main/views.py
+++ ExcelCalculate/main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from random import *
+from . models import *
+from sendEmail.views import *
 
 # Create your views here.
 def index(request):
@@ -7,6 +10,23 @@ def index(request):
 def signup(request):
     return render(request, 'main/signup.html')
 
+def join(request):
+    print(request)
+    name = request.POST['signupName']
+    email = request.POST['signupEmail']
+    pw = request.POST['signupPW']
+    user = User(user_name = name, user_email = email, user_password = pw)
+    user.save()
+    code = randint(1000,9999)
+    response = redirect('main_verifyCode')
+    response.set_cookie('code',code)
+    response.set_cookie('user_id',user.id)
+    send_result = send(email,code)
+    if send_result:
+        return response
+    else:
+        return HttpResponse('이메일 발송에 실패했습니다.')
+
 def signin(request):
     return render(request, 'main/signin.html')
 
@@ -14,7 +34,19 @@ def verifyCode(request):
     return render(request, 'main/verifyCode.html')
 
 def verify(request):
-    return redirect('main_index')
+    user_code = request.POST['verifyCode']
+    cookie_code = request.COOKIES.get('code')
+    if user_code == cookie_code:
+        user = User.objects.get(id=request.COOKIES.get('user_id'))
+        user.user_validate = 1
+        user.save()
+        response = redirect('main_index')
+        response.delete_cookie('code')
+        response.delete_cookie('user_id')
+        response.set_cookie('user', user)
+        return response
+    else:
+        redirect('main_verifyCode')
 
 def result(request):
     return render(request, 'main/result.html')
